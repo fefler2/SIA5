9fdd8b08b50ca66b0518410dc6462caf68f92f54
giciarz412 <tomaszfefler1990@gmail.com>
2018-05-02 17:04:54 +0200

    pierwszy Hello World

--- src/main/java/com/example/SIA5/others/Kontroller.java
+++ src/main/java/com/example/SIA5/others/Kontroller.java
@@ -2,17 +2,22 @@ package com.example.SIA5.others;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class Kontroller {
 
-    @RequestMapping("/")
-    public String a(){
-        return "Aaa";
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
     @RequestMapping("/a")
